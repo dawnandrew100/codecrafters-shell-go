5d04b377e23ecd6b9fe196c7e834e571cf7f23ad
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-30 21:34:50 -0400

    how about now?

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"os"
     "strings"
+    "slices"
 )
 
-var built_ins string = "exit 0, echo, type"
+built_ins := []string{"exit 0", "echo", "type"} 
 
 func main() {
     for {
@@ -32,7 +33,7 @@ func responseHandler(incoming string) {
     case strings.HasPrefix(incoming, "type"):
         check := strings.TrimPrefix(incoming, "type ")
         check = strings.TrimSpace(check)
-        if strings.Contains(built_ins, "check"){
+        if slices.Contains(built_ins, "check"){
             fmt.Fprint(os.Stdout, check+" is a shell builtin\n")
         } else {
             fmt.Fprint(os.Stdout, check+" not found\n")
