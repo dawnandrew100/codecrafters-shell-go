2711d05694882ccd0c9f84140437a35d178bd2ee
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-30 20:22:39 -0400

    Hopefully we learnt from out server project

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,9 +8,15 @@ import (
 )
 
 func main() {
-    fmt.Fprint(os.Stdout, "$ ")
+    for {
+        fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-    input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-    fmt.Fprint(os.Stdout, strings.TrimRight(input, "\n")+": command not found\n")
+	    // Wait for user input
+        input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+    }
+    go responseHandler(input)
+}
+
+func responseHandler(incoming string) {
+    fmt.Fprint(os.Stdout, strings.TrimRight(incoming, "\n")+": command not found\n")
 }
