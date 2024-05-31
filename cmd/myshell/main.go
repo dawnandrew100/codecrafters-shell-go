f1abadba0f8af7c6b62383a587cc74267c9e8148
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-30 21:21:17 -0400

    slices.contains

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"os"
     "strings"
+    "slices"
 )
+
+var built_ins = [...]string{"exit 0", "echo", "type"} 
 
 func main() {
     for {
@@ -26,6 +29,13 @@ func responseHandler(incoming string) {
     case strings.HasPrefix(incoming, "echo"):
         message := strings.TrimPrefix(incoming, "echo ")
         fmt.Fprint(os.Stdout, message+"\n")
+
+    case strings.HasPrefix(incoming, "type"):
+        check := strings.TrimPrefix(incoming, "type ")
+        if slices.Contains(built_ins, "check"){
+            fmt.Fprint(os.Stdout, check+" is a shell builtin\n")
+        } else {
+            fmt.Fprint(os.Stdout, check+" not found\n"
 
     default:
         fmt.Fprint(os.Stdout, incoming +": command not found\n")
