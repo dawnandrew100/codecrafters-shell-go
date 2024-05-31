632c86e8bc7e3130d075779045791625cd6b3a50
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-30 21:37:05 -0400

    Welp

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -33,8 +33,7 @@ func responseHandler(incoming string) {
 
     case strings.HasPrefix(incoming, "type"):
         check := strings.TrimPrefix(incoming, "type ")
-        check = strings.TrimSpace(check)
-        if slices.Contains(built_ins, "check"){
+        if slices.Contains(built_ins, check){
             fmt.Fprint(os.Stdout, check+" is a shell builtin\n")
         } else {
             fmt.Fprint(os.Stdout, check+" not found\n")
