17d2cfa1aaa7021660c0498c5d8db12e9260fd92
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-30 20:15:41 -0400

    Command not found

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+    "strings"
 )
 
 func main() {
     fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+    input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+    fmt.Fprint(os.Stdout, strings.TrimRight(input, "\n")+": command not found"
 }
